318b8604cabbb7b9eeac6640a9fb442ebde33d8b
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-07-06 21:50:17 -0700

    Adds SET, GET, and DELETE commands

---
+++ data/commands.go
@@ -0,0 +1,30 @@
+package data
+
+import "errors"
+
+var (
+	ErrKeyNotFound = errors.New("key does not exist")
+)
+
+func (db *Database) Set(key, val string) {
+	db.data[key] = val
+}
+
+func (db *Database) Get(key string) (string, error) {
+	val, exists := db.data[key]
+	if !exists {
+		return "", ErrKeyNotFound
+	}
+
+	return val, nil
+}
+
+func (db *Database) Delete(key string) error {
+	_, exists := db.data[key]
+	if !exists {
+		return ErrKeyNotFound
+	}
+
+	delete(db.data, key)
+	return nil
+}
--- data/types.go
+++ data/types.go
@@ -3,3 +3,8 @@ package data
 type Database struct {
 	data map[string]string
 }
+
+type Command struct {
+	Name string
+	Args []string
+}
