389f5a0032e4c53691c73f503d37974c85143d6e
Jimmy Kuang <66882632+JimmyKuangg@users.noreply.github.com>
2026-07-10 17:39:02 -0700

    Adds lock to ResetAOF (#8)

--- storage/reset.go
+++ storage/reset.go
@@ -6,6 +6,9 @@ import (
 )
 
 func ResetAOF() error {
+	storageMu.Lock()
+	defer storageMu.Unlock()
+
 	aofPath := filepath.Join(storageDir, aofFile)
 	file, err := os.OpenFile(aofPath, os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
