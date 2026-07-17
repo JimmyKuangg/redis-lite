0604029d107e72f49adb6a5a0cddc6d839be056e
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-07-16 18:09:35 -0700

    Adds string parser

--- commands/parser.go
+++ commands/parser.go
@@ -9,7 +9,10 @@ import (
 func ParseCommand(input string) (data.Command, error) {
 	var command data.Command
 
-	args := strings.Fields(input)
+	args, err := splitArgs(input)
+	if err != nil {
+		return command, err
+	}
 
 	if len(args) == 0 {
 		return command, fmt.Errorf("Please enter a command")
@@ -19,4 +22,48 @@ func ParseCommand(input string) (data.Command, error) {
 		Name: strings.ToUpper((args[0])),
 		Args: args[1:],
 	}, nil
+}
+
+func splitArgs(input string) ([]string, error) {
+	var args []string
+	var current strings.Builder
+	inQuotes := false
+	escaped := false
+
+	for _, r := range input {
+		switch {
+		case escaped:
+			current.WriteRune(r)
+			escaped = false
+
+		case r == '\\':
+			escaped = true
+
+		case r == '"':
+			inQuotes = !inQuotes
+
+		case (r == ' ' || r == '\t' || r == '\n' || r == '\r') && !inQuotes:
+			if current.Len() > 0 {
+				args = append(args, current.String())
+				current.Reset()
+			}
+
+		default:
+			current.WriteRune(r)
+		}
+	}
+
+	if escaped {
+		return nil, fmt.Errorf("unfinished escape sequence")
+	}
+
+	if inQuotes {
+		return nil, fmt.Errorf("unterminated quoted string")
+	}
+
+	if current.Len() > 0 {
+		args = append(args, current.String())
+	}
+
+	return args, nil
 }
